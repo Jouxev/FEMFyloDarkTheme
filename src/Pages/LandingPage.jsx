cbe6d32f33ad62530e82bfc50d74fdd5cf4d3f89
Youcef Haroun <mr.hys9@aol.fr>
2022-04-15 05:29:43 +0100

    First Commit

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Container = styled.main``
+
+export const LandingPage = () => {
+
+    return (
+        <Container>
+            Landing Page
+        </Container>
+    )
+}
